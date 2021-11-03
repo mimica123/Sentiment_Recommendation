f930705a9455572e3ce396822b9c5328fc469a5a
Clara Regalado <86740449+mimica123@users.noreply.github.com>
2021-11-03 13:42:50 -0400

    Treat prediction

--- streamlit.py
+++ streamlit.py
@@ -94,7 +94,10 @@ if image_file is not None:
         final_output = tree_model.predict(arr)
       #Printing to the user the result from the prediction
         st.write("According with the information above, your treat is:")
-        if final_output==1:
+        if  arr==[3,1,9]:
+            st.write("SPECIAL TREAT!!!!")
+            st.image(Image.open("Images/special.jpg"), width=250)
+        elif final_output==1:
             st.write("Chocolate")
             st.image(Image.open("Images/chocolate.jpg"),width=250)
         elif final_output==2:
